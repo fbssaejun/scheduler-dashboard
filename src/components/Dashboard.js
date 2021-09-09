84ce321794c237fa81b73659ebd4280b65a3ed0c
Anthony Kim <fbssaejun@gmail.com>
2021-09-09 17:26:04 -0400

    add api requests using axios

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,13 @@ import React,  {Component}  from "react";
 import Loading from './Loading.js';
 import Panel from "./Panel.js";
 import classnames from "classnames";
+import axios from 'axios';
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay
+ } from "helpers/selectors";
 
 //Fake data
 
@@ -9,22 +16,22 @@ const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    getValue: getTotalInterviews
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    getValue: getLeastPopularTimeSlot
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    getValue: getMostPopularDay
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
+    getValue: getInterviewsPerDay
   }
 ];
 
@@ -51,20 +58,34 @@ class Dashboard extends Component {
     if (focused) {
       this.setState({ focused });
     }
-  }
 
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data
+      });
+    });
+
+  }
+  
   componentDidUpdate(previousProps, previousState) {
     if (previousState.focused !== this.state.focused) {
       localStorage.setItem("focused", JSON.stringify(this.state.focused));
     }
   }
-    
-
+  
+  
   render() {
     const dashboardClasses = classnames("dashboard", {
       "dashboard--focused": this.state.focused
-     });
-
+    });
+    
     if (this.state.loading) {
       return <Loading />
     }
